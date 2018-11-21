71f0cff311c542b62b9d13bf50216d9a07a763a7
이연경 <mnm1705@gmail.com>
2018-11-22 02:20:53 +0900

    manifest 수정 + url 스토리지에 저장 및 삭제

--- background.js
+++ background.js
@@ -1,7 +1,52 @@
+var url;
+
 chrome.browserAction.onClicked.addListener( () => {
 	console.log("왜 안대냐고");
 	chrome.tabs.create( {
 		url : "http://news.naver.com/"
+		
 	} );
+	addURLToStorage(url);
+	showStorageData('url');
 })
 
+whale.tabs.onCreated.addListener( function(tab) {
+		url = tab.url;
+		
+});
+whale.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
+		if(url === "") {
+			url = tab.url;
+			console.log(url);
+		}
+});
+
+whale.tabs.onRemoved.addListener( ()=> {
+	removeStorageData('url');
+});
+
+function addURLToStorage(URLvalue) {
+	// 스토리지에 url 저장
+	whale.storage.local.set({
+		url : URLvalue
+	}, ()=> {
+		if(!whale.runtime.lastError) {
+			console.log("url saved");
+		}
+	});
+}
+
+function showStorageData(key) {
+	whale.storage.local.get([key], function(result) {
+		console.log(result);
+	});
+}
+
+function removeStorageData(key) {
+	whale.storage.local.remove([key], ()=> {
+		if(!whale.runtime.lastError) {
+			console.log("url removed");
+		}
+		showStorageData(key);
+	});
+}
