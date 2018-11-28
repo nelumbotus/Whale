c2474a221596bd64d7faa146b2fede2854a4de32
이연경 <mnm1705@gmail.com>
2018-11-28 13:16:57 +0900

    url 클릭시 링크 가져오기

--- background.js
+++ background.js
@@ -8,8 +8,24 @@ whale.sidebarAction.onClicked.addListener( () => {
 
 
 whale.tabs.onRemoved.addListener( ()=> {
+	console.log("tab removed");
 	//removeStorageData('url');
 });
+
+document.body.onclick = function(e) {
+	// TODO : 현재 페이지가 검색엔진인지 검사하기
+
+	var target = e.target;
+	
+	var tagName = target.tagName.toLowerCase(); // 클릭한 오브젝트의 태그이름 얻기
+	console.log("tag name : " + target.tagName.toLowerCase());
+
+	// 태그가 <a>일 때 href가져오기
+	if(tagName == 'a') {
+		var href = target.getAttribute("href");
+		alert("hyperlink clicked. link is : " + href);
+	}
+}
 
 function addURLToStorage(URLvalue) {
 	// 스토리지에 url 저장
--- sidebar.js
+++ sidebar.js
@@ -2,6 +2,7 @@
 window.onload = function () {
 	//추가하기 버튼
 	document.getElementById("addBtn").onclick = function () {
+	  	// 추가하기를 누르면 현재 탭들 쿼리에서 0번째를 가져온다
 		whale.tabs.query({
 				"active": true,
 				"lastFocusedWindow": true
@@ -12,16 +13,15 @@ window.onload = function () {
 
 			});
 	}
+
 }
 
 function showDebugText(text) {
-	// 콘솔열기 귀찮아서 ㅜ log 찍는 div를 만들었어요.. 
+	// log 찍는 div를 만들었어요.. 
 	document.getElementById("forDebug").innerHTML = text;
 }
-
 function getKeywordFromURL(URL) {
 	var param; // 리턴할 파라미터
-	URL = decodeURIComponent(URL); // url 디코드 -> 한글 검색어 얻기 위해
 
 	if (URL.match("/search.naver?")) { //검색 창이 네이버일 때
 	  	var tempString = URL.substring(URL.indexOf('query=') + 6); // 검색어 앞부분 자르기
@@ -33,7 +33,22 @@ function getKeywordFromURL(URL) {
 		var tempString = URL.substring(URL.indexOf('q=') + 2); // 검색어 앞부분 자르기
 		var params = tempString.split('&'); // &으로 뒷부분 split
 		param = params[0]; // 잘린 params 중 처음 요소 얻기
-	}
+	} else return; // 검색 엔진이 아닌 경우 아무것도 리턴하지 않음 
 
+  	param = decodeURIComponent(param); // url 디코드
 	return param;
 }
+
+
+// 의도하지 않은 페이지 전환 막기 
+window.addEventListener(`dragover`, (evt = event) => {
+    evt.preventDefault();
+    evt.dataTransfer.effectAllowed = `none`;
+    evt.dataTransfer.dropEffect = `none`;
+}, false);
+
+window.addEventListener(`drop`, (evt = event) => {
+    evt.preventDefault();
+    evt.dataTransfer.effectAllowed = `none`;
+    evt.dataTransfer.dropEffect = `none`;
+}, false);
